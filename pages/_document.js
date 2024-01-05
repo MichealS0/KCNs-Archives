9a759e8c0d803b28c8302b26938222718250f852
Theron Aiolos <awaabkheiri7177@gmail.com>
2024-01-05 23:51:47 +0800

    Fix the analytics
    Fix the analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Document() {
   return (
     <Html lang="en">
+      <title>KCN's archive of pwned Hack The Box Machines</title>
       <Head />
       <body>
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   )
