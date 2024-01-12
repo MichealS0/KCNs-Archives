7fb16c9c44890d510681220763b1bc0e9b527407
Theron Aiolos <awaabkheiri7177@gmail.com>
2024-01-12 16:24:12 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function HomePage(){
                 <div className="w-3/5 h-[60%] flex flex-col text-center py-5 md:flex-row md:justify-center mx-auto">
                     <div className="my-auto textBackground p-4 rounded-md border-2 border-black">
                         <h1 className="text-3xl">KCN's Archives</h1>
-                        <p>Embark on a cyber odyssey with "CipherQuest"—a blog documenting a beginner's conquest of Hack The Box challenges. Join me on this riveting journey through triumphs and pitfalls. As a personal blog, CipherQuest shares experiences, not tutorials. Explore the thrill of HTB through a unique, narrative lens.</p>
+                        <p>Embark on a cyber odyssey with "KCN's Archives"—a blog documenting a beginner's conquest of Hack The Box challenges. Join me on this riveting journey through triumphs and pitfalls. As a personal blog, KCN's Archives shares experiences, not tutorials. Explore the thrill of HTB through a unique, narrative lens.</p>
                     </div>
                     <div className="my-auto">
                         <img src="/Retro Computer.png" />
